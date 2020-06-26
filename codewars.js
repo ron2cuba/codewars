f61bfb2793444bb711a7631b76e79c3957a7779e
ron2cuba <ronan.lenouvel@gmail.com>
2020-06-26 18:01:23 +0200

    unfinished cakes

--- codewars.js
+++ codewars.js
@@ -275,12 +275,22 @@ function solution(str) {
 // cakes({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 });
 // // must return 0
 // cakes({ apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }, { sugar: 500, flour: 2000, milk: 2000 });
+function isObjectsEquals(a, b) {
+  var isOK = true;
+  for (var prop in a) {
+    if (a[prop] !== b[prop]) {
+      isOK = false;
+      break;
+    }
+  }
+  return isOK;
+}
 
 function cakes(recipe, available) {
-  let possibleCake;
-  console.log(recipe, available);
-  if (recipe == available) {
+  //toDo tester les propriétés available par rapport a recipe
+  if (isObjectsEquals(recipe, available)) {
     return 1;
   }
-  return possibleCake;
 }
+
+//retourne sur master
